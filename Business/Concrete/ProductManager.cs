0e91a81f961103afe03da00b8f996010b56bc20c
AhmetZYTN <59735377+AhmetZYTN@users.noreply.github.com>
2021-02-01 02:22:46 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _ProductDal;
+        private InMemoryProductDal ınMemoryProductDal;
+        private InMemoryProductDal ınMemoryProductDal1;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _ProductDal = productDal;
+        }
+
+        public ProductManager(InMemoryProductDal ınMemoryProductDal1)
+        {
+            this.ınMemoryProductDal1 = ınMemoryProductDal1;
+        }
+
+        //  public ProductManager(InMemoryProductDal ınMemoryProductDal) => this.ınMemoryProductDal = ınMemoryProductDal;
+
+        public List<Product> GetAll()
+        {
+            //iş kodları
+            return _ProductDal.GetAll();
+        }
+    }
+}
